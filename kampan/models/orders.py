4e037a3f1fba8b141a44cdd71959cdfb3b4240d7
jakkapat.j7 <jakkapat.j7@mail.com>
2021-11-13 14:39:59 +0700

    edit filename,add more attribute and update attribute

--- kampan/models/orders.py
+++ kampan/models/orders.py
@@ -3,25 +3,27 @@ import mongoengine as me
 class Itemregisteration(me.Document):
     meta = {"collection": "itemregister"}
 
-    ItemData = me.DocumentField(Item)
+    ItemData = me.ReferenceField(Item)
     Description = me.StringField()
 
-    Supplier = me.DocumentField(Supplier)
-    User = me.DocumentField(User)
+    Supplier = me.ReferenceField("Supplier",dbref=True)
+    User = me.ReferenceField("User",dbref=True)
     Quantity = me.FloatField(required=True)
     Price = me.FloatField(required=True)
-    Status = me.DocumentField(Status)
+    Status = me.ReferenceField("Status",dbref=True)
     Date = me.StringField(required=True)
+    expiration_date = me.StringField(required=True)
+    location = me.StringField(required=True)
 
 class Checkout(me.Document):
     meta = {"collection": "itemcheckout"}
 
-    ItemData = me.DocumentField(Item)
+    ItemData = me.ReferenceField(Item)
     Description = me.StringField()
 
-    Supplier = me.DocumentField(Supplier)
-    User = me.DocumentField(User)
+    Supplier = me.ReferenceField(Supplier)
+    User = me.ReferenceField(User)
     Quantity = me.FloatField(required=True)
     Price = me.FloatField(required=True)
-    Status = me.DocumentField(Status)
+    Status = me.ReferenceField(Status)
     Date = me.StringField(required=True)
