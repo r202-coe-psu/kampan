fb8033984f9ba648e9bb903c51761dacf88b1d5a
jakkapat.j7 <jakkapat.j7@mail.com>
2021-11-13 14:43:34 +0700

    update attribute

--- kampan/models/orders.py
+++ kampan/models/orders.py
@@ -3,27 +3,27 @@ import mongoengine as me
 class Itemregisteration(me.Document):
     meta = {"collection": "itemregister"}
 
-    ItemData = me.ReferenceField(Item)
-    Description = me.StringField()
+    item_data = me.ReferenceField("Item",dbref=True)
+    description = me.StringField()
 
-    Supplier = me.ReferenceField("Supplier",dbref=True)
-    User = me.ReferenceField("User",dbref=True)
-    Quantity = me.FloatField(required=True)
-    Price = me.FloatField(required=True)
-    Status = me.ReferenceField("Status",dbref=True)
-    Date = me.StringField(required=True)
+    supplier = me.ReferenceField("Supplier",dbref=True)
+    user = me.ReferenceField("User",dbref=True)
+    quantity = me.FloatField(required=True)
+    price = me.FloatField(required=True)
+    status = me.ReferenceField("Status",dbref=True)
+    date = me.StringField(required=True)
     expiration_date = me.StringField(required=True)
     location = me.StringField(required=True)
 
 class Checkout(me.Document):
     meta = {"collection": "itemcheckout"}
 
-    ItemData = me.ReferenceField(Item)
-    Description = me.StringField()
+    item_data = me.ReferenceField("Item",dbref=True)
+    description = me.StringField()
 
-    Supplier = me.ReferenceField(Supplier)
-    User = me.ReferenceField(User)
-    Quantity = me.FloatField(required=True)
-    Price = me.FloatField(required=True)
-    Status = me.ReferenceField(Status)
-    Date = me.StringField(required=True)
+    supplier = me.ReferenceField("Supplier",dbref=True)
+    user = me.ReferenceField("User",dbref=True)
+    quantity = me.FloatField(required=True)
+    price = me.FloatField(required=True)
+    status = me.ReferenceField("Status",dbref=True)
+    date = me.StringField(required=True)
