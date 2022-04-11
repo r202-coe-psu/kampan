238072cfdccf624bc6a10b4c18a5432b323c1ed8
Thanathip Limna <boatkrap@gmail.com>
2022-04-11 21:32:05 +0700

    fix lib

--- kampan/web/acl.py
+++ kampan/web/acl.py
@@ -1,13 +1,11 @@
 from flask import redirect, url_for, request
 from flask_login import current_user, LoginManager, login_url
-from flask_principal import Principal, identity_loaded, Identity, UserNeed, RoleNeed
 from werkzeug.exceptions import Forbidden
 from . import models
 
 from functools import wraps
 
 login_manager = LoginManager()
-principals = Principal()
 
 
 def init_acl(app):
@@ -18,7 +16,7 @@ def init_acl(app):
         return unauthorized_callback()
 
 
-def roles_requred(roles):
+def roles_required(roles):
     def wrapper(func):
         @wraps(func)
         def wrapped(*args, **kwargs):
@@ -45,25 +43,3 @@ def unauthorized_callback():
         return response
 
     return redirect(url_for("accounts.login"))
-
-
-@principals.identity_loader
-def load_identity_when_session_expires():
-    if hasattr(current_user, "id"):
-        return Identity(current_user)
-
-
-@identity_loaded.connect
-def on_identity_loaded(sender, identity):
-    # Set the identity user object
-    identity.user = current_user
-
-    # Add the UserNeed to the identity
-    if hasattr(current_user, "id"):
-        identity.provides.add(UserNeed(current_user.id))
-
-    # Assuming the User model has a list of roles, update the
-    # identity with the roles that the user provides
-    if hasattr(current_user, "roles"):
-        for role in current_user.roles:
-            identity.provides.add(RoleNeed(role))
