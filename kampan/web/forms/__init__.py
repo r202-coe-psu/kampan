434965b115e6eaa03a9c9dcc32da48f7ef729238
Napat Chalerwong <bomzyx123456@gmail.com>
2022-04-29 21:25:48 +0700

    Add item_orders

--- kampan/web/forms/__init__.py
+++ kampan/web/forms/__init__.py
@@ -2,5 +2,6 @@ from . import items
 from . import warehouses
 from . import suppliers
 from . import item_positions
+from . import item_orders
 from . import item_registers
 from . import item_checkouts
