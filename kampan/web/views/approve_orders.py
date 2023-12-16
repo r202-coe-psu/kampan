afeaa9b602d30bc13ca3c7cb21124fd843d11884
sattarawut <panpon9988@gmail.com>
2023-12-16 21:52:26 +0700

    change status to approval_status

--- kampan/web/views/approve_orders.py
+++ kampan/web/views/approve_orders.py
@@ -11,11 +11,10 @@ from kampan.web.views.lost_breaks import check_in_time
 
 module = Blueprint("approve_orders", __name__, url_prefix="/approve_orders")
 
-def check_in_time(created_date, calendar_select,calendar_end):
+
+def check_in_time(created_date, calendar_select, calendar_end):
     print(
-        created_date, 
-        calendar_select, 
-        calendar_select <= created_date <= calendar_end
+        created_date, calendar_select, calendar_select <= created_date <= calendar_end
     )
     if calendar_select <= created_date <= calendar_end:
         return True
@@ -23,35 +22,36 @@ def check_in_time(created_date, calendar_select,calendar_end):
         return False
 
 
-@module.route("/",methods=["GET","POST"])
+@module.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     if "admin" in current_user.roles or "supervisor" in current_user.roles:
-        orders = models.OrderItem.objects(status="pending")
+        orders = models.OrderItem.objects(approval_status="pending")
 
         form = forms.inventories.InventoryForm()
-        
+
         return render_template(
             "/approve_orders/index.html",
             orders=orders,
             form=form,
             calendar_select=form.calendar_select.data,
-            calendar_end = form.calendar_end.data,
+            calendar_end=form.calendar_end.data,
             check_in_time=check_in_time,
         )
     return redirect(url_for("dashboard.daily_dashboard"))
+
 
 @module.route("<order_id>", methods=["GET"])
 def approve(order_id):
     order = models.OrderItem.objects.get(id=order_id)
     checkout_items = models.CheckoutItem.objects(order=order)
-    
-    order.status = "approved"
+
+    order.approval_status = "approved"
     order.save()
 
     for checkout in checkout_items:
-        if checkout.order.status == "approved":
-            checkout.status = "approved"
+        if checkout.order.approval_status == "approved":
+            checkout.approval_status = "approved"
             checkout.save()
-    
+
     return redirect(url_for("approve_orders.index"))
