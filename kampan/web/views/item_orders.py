6697e331c8d625d276a9ee20e3dd3e1a8ec5bc95
sattarawut <panpon9988@gmail.com>
2023-12-20 03:51:28 +0700

    edit search date

--- kampan/web/views/item_orders.py
+++ kampan/web/views/item_orders.py
@@ -1,6 +1,6 @@
 from calendar import calendar
 from pyexpat import model
-from flask import Blueprint, render_template, redirect, url_for,request
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from kampan.web import forms
 from kampan import models
@@ -18,51 +18,67 @@ import mongoengine as me
 
 import datetime
 
-module = Blueprint('item_orders', __name__, url_prefix='/item_orders')
+module = Blueprint("item_orders", __name__, url_prefix="/item_orders")
 
-def check_in_time(created_date, calendar_select,calendar_end):
-    print(created_date, calendar_select, calendar_select <= created_date <= calendar_end)
+
+def check_in_time(created_date, calendar_select, calendar_end):
+    print(
+        created_date, calendar_select, calendar_select <= created_date <= calendar_end
+    )
     if calendar_select <= created_date <= calendar_end:
         return True
     else:
         return False
 
-@module.route('/',methods=["GET","POST"])
+
+@module.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     orders = models.OrderItem.objects()
 
-    form = forms.inventories.InventoryForm()
+    form = forms.inventories.SearchStartEndDateForm()
+    if form.start_date.data == None and form.end_date.data != None:
+        orders = orders.filter(
+            created_date__lte=form.end_date.data,
+        )
+
+    elif form.start_date.data and form.end_date.data == None:
+        orders = orders.filter(
+            created_date__gte=form.start_date.data,
+        )
+
+    elif form.start_date.data != None and form.end_date.data != None:
+        orders = orders.filter(
+            created_date__gte=form.start_date.data,
+            created_date__lte=form.end_date.data,
+        )
     return render_template(
         "/item_orders/index.html",
         orders=orders,
         form=form,
-        calendar_select=form.calendar_select.data,
-        calendar_end = form.calendar_end.data,
-        check_in_time=check_in_time,
-        )
+    )
 
 
-@module.route('/order', methods=["GET", "POST"])
+@module.route("/order", methods=["GET", "POST"])
 @login_required
 def order():
     items = models.Item.objects()
     form = forms.item_orders.OrderItemForm()
     if not form.validate_on_submit():
         return render_template(
-            '/item_orders/order.html',
-            form=form,           
-             )
+            "/item_orders/order.html",
+            form=form,
+        )
 
     order = models.OrderItem()
-        
+
     form.populate_obj(order)
     order.user = current_user._get_current_object()
-    
-    order.save()
-    
 
-    return redirect(url_for('item_orders.index'))
+    order.save()
+
+    return redirect(url_for("item_orders.index"))
+
 
 @module.route("/<order_id>/edit", methods=["GET", "POST"])
 @login_required
@@ -81,6 +97,7 @@ def edit(order_id):
     order.save()
 
     return redirect(url_for("item_orders.index"))
+
 
 @module.route("/<order_id>/delete")
 @login_required
