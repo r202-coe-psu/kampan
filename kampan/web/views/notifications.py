e6a0537da947072a01f5d366ed9c8f3d310e5af1
sattarawut <panpon9988@gmail.com>
2023-12-25 06:51:11 +0700

    edit notification

--- kampan/web/views/notifications.py
+++ kampan/web/views/notifications.py
@@ -16,36 +16,47 @@ def index():
     inventories = models.Inventory.objects(status="active")
     checkouts = models.CheckoutItem.objects(status="active")
 
-    total_values = 0
     notifications = []
-    checkout_trend_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-    for checkout in checkouts:
-        date = checkout.checkout_date
-        now = datetime.datetime.now()
-        if int(now.strftime("%Y")) - int(date.strftime("%Y")) == 0:
-            month = int(date.strftime("%m")) - 1
-            checkout_trend_month[month] += checkout.quantity
-            total_values += checkout.price
+    items = models.Item.objects(status="active")
+    for item in items:
+        if item.minimum > item.get_items_quantity():
+            notifications.append(item)
+    # pipeline = [
+    #     {"$match": {"status": "active"}},
+    #     {"$group": {"_id": "$item", "total": {"$sum": "$remain"}}},
+    # ]
+    # inventories = models.Inventory.objects().aggregate(pipeline)
+    # for inventory in inventories:
+    #     print(inventory["_id"])
+    # total_values = 0
+    # checkout_trend_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # for checkout in checkouts:
+    #     date = checkout.checkout_date
+    #     now = datetime.datetime.now()
+    #     if int(now.strftime("%Y")) - int(date.strftime("%Y")) == 0:
+    #         month = int(date.strftime("%m")) - 1
+    #         checkout_trend_month[month] += checkout.quantity
+    #         total_values += checkout.price
 
-    for inventory in inventories:
-        # If inventory remain is less than 25%
-        if inventory.item.minimum:
-            if inventory.remain <= inventory.item.minimum:
-                if inventory.notification_status == True:
-                    notifications.append(inventory)
+    # for inventory in inventories:
+    #     # If inventory remain is less than 25%
+    #     if inventory.item.minimum:
+    #         if inventory.remain <= inventory.item.minimum:
+    #             if inventory.notification_status == True:
+    #                 notifications.append(inventory)
 
-    print(notifications)
+    # print(notifications)
     return render_template(
         "/notifications/index.html",
         notifications=notifications,
     )
 
 
-@module.route("/<inventory_id>/set_status")
-def set_status(inventory_id):
-    inventory = models.Inventory.objects().get(id=inventory_id)
-    inventory.notification_status = False
-    inventory.save()
+@module.route("/<item_id>/set_status")
+def set_status(item_id):
+    item = models.Item.objects(id=item_id).first()
+    item.notification_status = False
+    item.save()
 
     return redirect(url_for("notifications.index"))
