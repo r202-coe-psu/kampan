071000892411447da53c974629cc986417370cb9
sattarawut <panpon9988@gmail.com>
2024-01-03 00:21:19 +0700

    add pagination

--- kampan/web/views/notifications.py
+++ kampan/web/views/notifications.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from kampan.web import forms
 from kampan import models
 import mongoengine as me
+from flask_mongoengine import Pagination
 
 import datetime
 
@@ -13,43 +14,17 @@ subviews = []
 @module.route("/")
 @login_required
 def index():
-    inventories = models.Inventory.objects(status="active")
-    checkouts = models.CheckoutItem.objects(status="active")
-
     notifications = []
 
     items = models.Item.objects(status="active")
     for item in items:
         if item.minimum > item.get_items_quantity():
             notifications.append(item)
-    # pipeline = [
-    #     {"$match": {"status": "active"}},
-    #     {"$group": {"_id": "$item", "total": {"$sum": "$remain"}}},
-    # ]
-    # inventories = models.Inventory.objects().aggregate(pipeline)
-    # for inventory in inventories:
-    #     print(inventory["_id"])
-    # total_values = 0
-    # checkout_trend_month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # for checkout in checkouts:
-    #     date = checkout.checkout_date
-    #     now = datetime.datetime.now()
-    #     if int(now.strftime("%Y")) - int(date.strftime("%Y")) == 0:
-    #         month = int(date.strftime("%m")) - 1
-    #         checkout_trend_month[month] += checkout.quantity
-    #         total_values += checkout.price
-
-    # for inventory in inventories:
-    #     # If inventory remain is less than 25%
-    #     if inventory.item.minimum:
-    #         if inventory.remain <= inventory.item.minimum:
-    #             if inventory.notification_status == True:
-    #                 notifications.append(inventory)
-
-    # print(notifications)
+    page = request.args.get("page", default=1, type=int)
+    paginated_notifications = Pagination(notifications, page=page, per_page=10)
     return render_template(
         "/notifications/index.html",
-        notifications=notifications,
+        paginated_notifications=paginated_notifications,
     )
 
 
