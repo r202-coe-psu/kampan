79ae2417202d69c568a591d5ea046883931578bb
sattarawut <panpon9988@gmail.com>
2024-01-20 18:09:14 +0700

    add citizen_id

--- kampan/models/users.py
+++ kampan/models/users.py
@@ -2,6 +2,12 @@ import mongoengine as me
 import datetime
 
 from flask_login import UserMixin
+from flask import url_for
+
+
+class UserSetting(me.EmbeddedDocument):
+    current_organization = me.ReferenceField("Organization", dbref=True)
+    updated_date = me.DateTimeField(required=True, default=datetime.datetime.now)
 
 
 class User(me.Document, UserMixin):
@@ -12,14 +18,20 @@ class User(me.Document, UserMixin):
     last_name = me.StringField(required=True, max_length=256)
     status = me.StringField(required=True, default="active")
     roles = me.ListField(me.StringField(), default=["user"])
-    student_id = me.StringField(min_length=5, max_length=64)
-    organization = me.StringField(min_length=3, max_length=64)
+    citizen_id = me.StringField(max_length=13)
+
     picture_url = me.StringField(max_length=500)
+    picture = me.ImageField(
+        thumbnail_size=(800, 600, True), collection_name="user_picture"
+    )
+
     biography = me.StringField(max_length=500)
     created_date = me.DateTimeField(required=True, default=datetime.datetime.now)
     updated_date = me.DateTimeField(
         required=True, default=datetime.datetime.now, auto_now=True
     )
+
+    user_setting = me.EmbeddedDocumentField("UserSetting", default=UserSetting)
     metadata = me.DictField()
     resources = me.DictField()
 
@@ -37,3 +49,12 @@ class User(me.Document, UserMixin):
 
     def get_name(self):
         return self.first_name + " " + self.last_name
+
+    def get_picture(self):
+        if self.picture:
+            return url_for(
+                "accounts.picture", user_id=self.id, filename=self.picture.filename
+            )
+        # if "google" in self.resources:
+        #     return self.resources["google"].get("picture", "")
+        # return url_for("static", filename="images/user.png")
