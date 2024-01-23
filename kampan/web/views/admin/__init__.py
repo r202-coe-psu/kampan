e13d8a32d860811e749865e498ef1a950de13389
sattarawut <panpon9988@gmail.com>
2024-01-23 16:24:56 +0700

    add admin index

--- kampan/web/views/admin/__init__.py
+++ kampan/web/views/admin/__init__.py
@@ -1,5 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
 import datetime
-
+from ... import acl
 
 module = Blueprint("admin", __name__, url_prefix="/admin")
+
+
+@module.route("/")
+@acl.roles_required("admin")
+def index():
+    return render_template(
+        "/admin/dashboard/index-admin.html",
+    )
