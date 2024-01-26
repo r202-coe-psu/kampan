84918cd2b38aa3c24fa3275174f47b50312d9b33
sattarawut <panpon9988@gmail.com>
2024-01-26 19:31:32 +0700

    add EmailTemplate

---
+++ kampan/models/email_templates.py
@@ -0,0 +1,55 @@
+import mongoengine as me
+import bson
+import datetime
+
+from jinja2 import Environment, PackageLoader, select_autoescape, Template
+
+import io
+import base64
+import markdown
+
+EMAIL_TYPE = [
+    ("participant", "Pariticpant"),
+    ("endorser_required_endorsement", "Endorser Required Endorsement"),
+    ("endorser_without_endorsement", "Endorser Without Endorsement"),
+]
+
+
+class EmailTemplate(me.Document):
+    meta = {"collection": "email_templates"}
+
+    name = me.StringField(require=True, max_length=255)
+    subject = me.StringField(required=True, max_length=255)
+    body = me.StringField(required=True)
+
+    type = me.StringField(
+        required=True,
+        choices=EMAIL_TYPE,
+    )
+
+    organization = me.ReferenceField("Organization", required=True, dbref=True)
+    is_default = me.BooleanField(
+        require=True,
+        default=False,
+        false_values=(
+            "False",
+            "false",
+        ),
+        true_values=(
+            "True",
+            "true",
+        ),
+    )
+    owner = me.ReferenceField("User", dbref=True, required=True)
+    last_updated_by = me.ReferenceField("User", dbref=True, required=True)
+
+    created_date = me.DateTimeField(required=True, default=datetime.datetime.now)
+    updated_date = me.DateTimeField(
+        required=True, default=datetime.datetime.now, auto_now=True
+    )
+
+    def get_subject(self):
+        return markdown.markdown(self.subject)
+
+    def get_body(self):
+        return markdown.markdown(self.body)
