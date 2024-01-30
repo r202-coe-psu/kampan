25d029bf348ac5dd5d49270c8881658f4a6f64f0
sattarawut <panpon9988@gmail.com>
2024-01-30 16:44:49 +0700

    update exclude

--- kampan/web/forms/suppliers.py
+++ kampan/web/forms/suppliers.py
@@ -9,19 +9,21 @@ BaseSupplierForm = model_form(
     models.Supplier,
     FlaskForm,
     exclude=[
-    "phone",
+        "phone",
+        "last_modifier",
+        "created_date",
+        "updated_date",
     ],
+    field_args={
+        "name": {"label": "ชื่อร้าน"},
+        "address": {"label": "ที่อยู่"},
+        "description": {"label": "คำอธิบาย"},
+        "tax_id": {"label": "เลขกำกับภาษี"},
+        "contact": {"label": "ช่องทางการติดต่อ"},
+        "email": {"label": "อีเมลล์"},
+    },
+)
 
-field_args={
-    "name" :{"label":"ชื่อร้าน"},
-    "address" : {"label": "ที่อยู่"},
-    "description": {"label": "คำอธิบาย"},
-    "tax_id": {"label": "เลขกำกับภาษี"},
-    "contact": {"label": "ช่องทางการติดต่อ"},
-    "email": {"label": "อีเมลล์"},
-    }
-    )
 
 class SupplierForm(BaseSupplierForm):
     pass
-
