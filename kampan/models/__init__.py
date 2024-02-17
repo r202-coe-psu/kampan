4a048c4f7e9f5bcb5524e87c196aa68a0d2534f5
sattarawut <panpon9988@gmail.com>
2024-02-17 22:04:38 +0700

    add init_mongoengine

--- kampan/models/__init__.py
+++ kampan/models/__init__.py
@@ -21,3 +21,15 @@ db = MongoEngine()
 
 def init_db(app):
     db.init_app(app)
+
+
+def init_mongoengine(settings):
+    import mongoengine as me
+
+    dbname = settings.get("MONGODB_DB")
+    host = settings.get("MONGODB_HOST", "localhost")
+    port = int(settings.get("MONGODB_PORT", "27017"))
+    username = settings.get("MONGODB_USERNAME", "")
+    password = settings.get("MONGODB_PASSWORD", "")
+
+    me.connect(db=dbname, host=host, port=port, username=username, password=password)
