e804fa5e7d062cc37d91e223e68584f9915f8859
sattarawut <panpon9988@gmail.com>
2024-02-17 22:05:32 +0700

    add utils config

---
+++ kampan/utils/config.py
@@ -0,0 +1,27 @@
+import os
+import flask
+import logging
+
+logger = logging.getLogger(__name__)
+
+settings = None
+
+
+def get_settings():
+    global settings
+
+    if not settings:
+        filename = os.environ.get("KAMPAN_SETTINGS", None)
+
+        # if filename is None:
+        #     print('This program require NOKKHUM_SETTINGS environment')
+
+        logger.debug(f"setting environment file: {filename}")
+
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../")
+
+        settings = flask.config.Config(file_path)
+        settings.from_object("kampan.default_settings")
+        settings.from_envvar("KAMPAN_SETTINGS", silent=True)
+
+    return settings
