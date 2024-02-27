5139eb7a7d3cc34dbe01d1ca1b9f10b4fe421941
sattarawut <panpon9988@gmail.com>
2024-02-27 22:14:00 +0700

    add validate error

--- kampan/utils/inventories.py
+++ kampan/utils/inventories.py
@@ -18,8 +18,8 @@ INVENTORY_HEADER = [
 
 def process_inventory_engagement(inventory_engagement_file):
     df = pd.read_excel(inventory_engagement_file.file)
-    sheet_names = df.keys()
-    print(sheet_names)
+
+    # print(sheet_names)
     df.columns = df.columns.str.strip()
     df = df.dropna(subset=["บาร์โค้ด"])
     organization = models.Organization.objects(
@@ -27,14 +27,13 @@ def process_inventory_engagement(inventory_engagement_file):
         status="active",
     ).first()
     for idx, row in df.iterrows():
-
         item = models.Item.objects(
             name=row["ชื่ออุปกรณ์"],
             barcode_id=str(row["บาร์โค้ด"]),
             status="active",
             organization=organization,
         ).first()
-        print("----->", item)
+
         warehouse = models.Warehouse.objects(
             status="active",
             name=row["คลังอุปกรณ์"],
@@ -49,8 +48,6 @@ def process_inventory_engagement(inventory_engagement_file):
             status="pending",
             registration=inventory_engagement_file.registration,
             item=item,
-            warehouse=warehouse,
-            position=position,
             organization=organization,
         ).first()
         if inventory:
@@ -58,6 +55,8 @@ def process_inventory_engagement(inventory_engagement_file):
             inventory.quantity = row["จำนวน (ชุด)"] * item.piece_per_set
             inventory.remain = row["จำนวน (ชุด)"] * item.piece_per_set
             inventory.price = row["ราคา (ชุดละ)"]
+            inventory.warehouse = warehouse
+            inventory.position = position
         else:
             inventory = models.Inventory()
             inventory.status = "pending"
@@ -74,3 +73,47 @@ def process_inventory_engagement(inventory_engagement_file):
     inventory_engagement_file.status = "completed"
     inventory_engagement_file.updated_date = datetime.datetime.now()
     inventory_engagement_file.save()
+
+
+def check_columns_file(df, default_columns):
+    if len(default_columns) != len(df.columns):
+        return "จำนวน Columns ของไฟล์ไม่ถูกต้อง"
+    for column in df.columns:
+        if column not in default_columns:
+            return "ชื่อ Columns ของไฟล์ไม่ถูกต้อง"
+
+
+def check_int_value(df):
+    for value in df:
+        if type(value) != int:
+            print("error-------")
+            return f"มีบางประเภทข้อมูลใน Columns ชื่อ '{df.name}' ไม่เป็นจำนวนเต็มบวก"
+        else:
+            if value < 0:
+                return f"มีบางประเภทข้อมูลใน Columns ชื่อ '{df.name}' ไม่เป็นจำนวนเต็มบวก"
+
+
+def check_float_value(df):
+    for value in df:
+        if type(value) == float or type(value) == int:
+            if value < 0:
+                return f"มีบางประเภทข้อมูลใน Columns ชื่อ '{df.name}' ไม่เป็นจำนวนทศนิยมบวก"
+        else:
+            return f"มีบางประเภทข้อมูลใน Columns ชื่อ '{df.name}' ไม่เป็นจำนวนทศนิยมบวก"
+
+
+def validate_inventory_engagement(inventory_engagement_file):
+    df = pd.read_excel(inventory_engagement_file.file)
+    df.columns = df.columns.str.strip()
+    invalide_column = check_columns_file(df, INVENTORY_HEADER)
+    if invalide_column:
+        return invalide_column
+
+    invalid_int_value = check_int_value(df["จำนวน (ชุด)"])
+    if invalid_int_value:
+        return invalid_int_value
+
+    invalid_float_value = check_float_value(df["ราคา (ชุดละ)"])
+    if invalid_float_value:
+        return invalid_float_value
+    print(df.dtypes)
