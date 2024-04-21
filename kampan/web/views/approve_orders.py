7871b970731dce81c0cb559a18d5a658e2abdb9e
sattarawut <panpon9988@gmail.com>
2024-04-21 22:15:04 +0700

    add admin function

--- kampan/web/views/approve_orders.py
+++ kampan/web/views/approve_orders.py
@@ -20,7 +20,7 @@ def index():
     organization = models.Organization.objects(
         id=organization_id, status="active"
     ).first()
-    orders = models.OrderItem.objects(approval_status="pending", status="active")
+    orders = models.OrderItem.objects(approval_status="pending", status="pending")
 
     form = forms.inventories.SearchStartEndDateForm()
     if form.start_date.data == None and form.end_date.data != None:
@@ -51,6 +51,44 @@ def index():
     )
 
 
+@module.route("/approve", methods=["GET", "POST"])
+@acl.organization_roles_required("admin")
+def admin_index():
+    organization_id = request.args.get("organization_id")
+    organization = models.Organization.objects(
+        id=organization_id, status="active"
+    ).first()
+    orders = models.OrderItem.objects(approval_status="pending", status="active")
+
+    form = forms.inventories.SearchStartEndDateForm()
+    if form.start_date.data == None and form.end_date.data != None:
+        orders = orders.filter(
+            created_date__lt=form.end_date.data,
+        )
+
+    elif form.start_date.data and form.end_date.data == None:
+        orders = orders.filter(
+            created_date__gte=form.start_date.data,
+        )
+
+    elif form.start_date.data != None and form.end_date.data != None:
+        orders = orders.filter(
+            created_date__gte=form.start_date.data,
+            created_date__lt=form.end_date.data,
+        )
+    page = request.args.get("page", default=1, type=int)
+    if form.start_date.data:
+        page = 1
+    paginated_orders = Pagination(orders, page=page, per_page=30)
+    return render_template(
+        "/approve_orders/admin_approve_index.html",
+        paginated_orders=paginated_orders,
+        form=form,
+        orders=orders,
+        organization=organization,
+    )
+
+
 @module.route("/<order_id>/approved_detail", methods=["GET", "POST"])
 @acl.organization_roles_required("admin", "endorser")
 def approved_detail(order_id):
@@ -72,28 +110,116 @@ def approved_detail(order_id):
     )
 
 
-@module.route("<order_id>", methods=["GET"])
+@module.route("/<order_id>/admin_approved_detail", methods=["GET", "POST"])
 @acl.organization_roles_required("admin", "endorser")
-def approve(order_id):
+def admin_approved_detail(order_id):
+    organization_id = request.args.get("organization_id")
+    organization = models.Organization.objects(
+        id=organization_id, status="active"
+    ).first()
+    error_message = request.args.get("error_message")
+
+    order = models.OrderItem.objects.get(id=order_id)
+    checkouts = models.CheckoutItem.objects(order=order, status="active")
+    page = request.args.get("page", default=1, type=int)
+    paginated_checkouts = Pagination(checkouts, page=page, per_page=30)
+    return render_template(
+        "/approve_orders/admin_approve_detail.html",
+        paginated_checkouts=paginated_checkouts,
+        order_id=order_id,
+        order=order,
+        checkouts=checkouts,
+        organization=organization,
+        error_message=error_message,
+    )
+
+
+@module.route("/<order_id>/endorser_approve", methods=["GET"])
+@acl.organization_roles_required("admin", "endorser")
+def endorser_approve(order_id):
+    organization_id = request.args.get("organization_id")
+
+    order = models.OrderItem.objects.get(id=order_id)
+    order.status = "active"
+    order.save()
+
+    return redirect(url_for("approve_orders.index", organization_id=organization_id))
+
+
+@module.route("/<order_id>/admin_approve", methods=["GET"])
+@acl.organization_roles_required("admin")
+def admin_approve(order_id):
     organization_id = request.args.get("organization_id")
 
     order = models.OrderItem.objects.get(id=order_id)
     checkout_items = models.CheckoutItem.objects(order=order, status="active")
+
+    for checkout in checkout_items:
+        checkout.status = "active"
+        # checkout.save()
+        item = checkout.item
+        quantity = (checkout.set_ * item.piece_per_set) + checkout.piece
+        # print("--->", quantity, item.get_amount_pieces())
+        if quantity > item.get_amount_pieces():
+            return redirect(
+                url_for(
+                    "approve_orders.admin_approved_detail",
+                    organization_id=organization_id,
+                    order_id=order_id,
+                    error_message=True,
+                )
+            )
+        inventories = models.Inventory.objects(item=item, remain__gt=0)
+        for inventory in inventories:
+            if inventory.remain >= quantity:
+                inventory.remain -= quantity
+                quantity = 0
+            else:
+                quantity -= inventory.remain
+                inventory.remain = 0
+
+            inventory.save()
+            checkout.inventories.append(inventory)
+            if quantity <= 0:
+                break
+        checkout.save()
     order.approval_status = "approved"
+    order.approved_date = datetime.datetime.now()
+    order.save()
+    return redirect(
+        url_for("approve_orders.admin_index", organization_id=organization_id)
+    )
+
+
+@module.route("/<order_id>/endorser_denied", methods=["GET"])
+@acl.organization_roles_required("admin", "endorser")
+def endorser_denied(order_id):
+    organization_id = request.args.get("organization_id")
+
+    order = models.OrderItem.objects.get(id=order_id)
+    order.status = "denied"
+    order.save()
+
+    return redirect(url_for("approve_orders.index", organization_id=organization_id))
+
+
+@module.route("/<order_id>/admin_denied", methods=["GET"])
+@acl.organization_roles_required("admin")
+def admin_denied(order_id):
+    organization_id = request.args.get("organization_id")
+
+    order = models.OrderItem.objects.get(id=order_id)
+    checkout_items = models.CheckoutItem.objects(order=order, status="active")
+    order.approval_status = "denied"
     order.save()
 
     for checkout in checkout_items:
-        checkout.approval_status = "approved"
+        checkout.status = "denied"
         checkout.save()
 
-    # return redirect(
-    #     url_for(
-    #         "email_templates.force_send_email",
-    #         order_id=order_id,
-    #         organization_id=organization_id,
-    #     )
-    # )
-    return redirect(url_for("approve_orders.index", organization_id=organization_id))
+    return redirect(
+        url_for("approve_orders.admin_index", organization_id=organization_id)
+    )
 
 
 @module.route("/item_checkouts", methods=["GET", "POST"])
