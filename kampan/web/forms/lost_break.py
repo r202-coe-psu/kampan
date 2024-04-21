0fe7d421395a238ceadc0077590497a2a9832366
sattarawut <panpon9988@gmail.com>
2024-04-21 21:59:17 +0700

    remove quantity from field_args

--- kampan/web/forms/lost_break.py
+++ kampan/web/forms/lost_break.py
@@ -8,19 +8,17 @@ from kampan import models
 BaseItemLostBreakForm = model_form(
     models.LostBreakItem,
     FlaskForm,
-    exclude=[
-        "created_date",
-        "user",
-        "lost_from",
-    ],
+    exclude=["created_date", "user", "lost_from", "quantity"],
     field_args={
         # "item": {"label": "ชื่ออุปกรณ์", "label_modifier": lambda i: i.name },
         "warehouse": {"label": "คลังอุปกรณ์", "label_modifier": lambda w: w.name},
         "description": {"label": "คำอธิบาย"},
-        "quantity": {"label": "จำนวนทั้งหมด"},
+        # "quantity": {"label": "จำนวนทั้งหมด"},
     },
 )
 
 
 class ItemLostBreakForm(BaseItemLostBreakForm):
     item = fields.SelectField(label="ชื่ออุปกรณ์")
+    set_ = fields.IntegerField(label="จำนวนชุด", default=0)
+    piece = fields.IntegerField(label="จำนวนชิ้น", default=0)
