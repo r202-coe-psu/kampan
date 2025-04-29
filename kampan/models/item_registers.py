cc5cdf67031193f946dbea2b44b213769b94a324
Pxngpond99 <panpon9988@gmail.com>
2025-04-29 20:16:04 +0700

    ✨ feat: feature add 1 inventory

--- kampan/models/item_registers.py
+++ kampan/models/item_registers.py
@@ -24,6 +24,15 @@ class RegistrationItem(me.Document):
         )
         if inventories:
             return [inventory.item.id for inventory in inventories]
+        return []
+
+    def get_item_in_bills(self):
+        inventories = models.Inventory.objects(
+            registration=self, status__ne="disactive"
+        )
+        if inventories:
+            return [inventory.item for inventory in inventories]
+        return []
 
     def get_quantity_of_item(self):
         quantiy_item = models.Inventory.objects(
---
+++ kampan/repositories/__init__.py
@@ -0,0 +1,1 @@
+from .item_registers import RegisterItemRepository
