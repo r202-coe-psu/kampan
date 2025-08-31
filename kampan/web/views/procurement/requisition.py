2f5dc3c7978a429545c16f386242d597c3049612
opecho <opecho0280@gmail.com>
2025-08-31 15:29:29 +0700

    feat: implement pagination for requisitions index page and enhance filtering options

--- kampan/web/views/procurement/requisition.py
+++ kampan/web/views/procurement/requisition.py
@@ -35,14 +35,22 @@ def index():
     if category:
         query["category"] = category
 
+    # Filter only items expiring within 7 days and status pending
     procurements = models.Procurement.objects(
-        **query, end_date__gte=today, end_date__lte=next_7
+        **query,
+        end_date__gte=today,
+        end_date__lte=next_7,
     ).order_by("end_date")
+
+    page = request.args.get("page", default=1, type=int)
+    per_page = request.args.get("per_page", default=4, type=int)
+    paginated_procurements = Pagination(procurements, page=page, per_page=per_page)
 
     category_choices = models.procurement.CATEGORY_CHOICES
     return render_template(
         "procurement/requisitions/index.html",
-        procurements=procurements,
+        procurements=paginated_procurements.items,
+        paginated_procurements=paginated_procurements,
         organization=organization,
         category_choices=category_choices,
         selected_category=category,
