1a56d82892351fe0aca02dc24f94ef927be457e4
saktanuthpeak <thanakrit.5122548@gmail.com>
2026-08-18 12:33:25 +0700

    feat: add motorcycle manaual

---
+++ kampan/web/views/vehicle_lending/motorcycle_manuals.py
@@ -0,0 +1,36 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    abort,
+)
+
+from kampan.web import acl
+from kampan import models
+
+module = Blueprint("motorcycle_manuals", __name__, url_prefix="/motorcycle_manuals")
+
+
+@module.route("", methods=["GET"])
+@acl.organization_roles_required(
+    "admin",
+    "endorser",
+    "staff",
+    "head",
+    "supervisor supplier",
+    "driver",
+    "director",
+    "manager",
+)
+def index():
+    organization_id = request.args.get("organization_id")
+    organization = models.Organization.objects(
+        id=organization_id, status="active"
+    ).first()
+    if not organization:
+        return abort(404)
+
+    return render_template(
+        "/vehicle_lending/motorcycle_manuals/index.html",
+        organization=organization,
+    )
